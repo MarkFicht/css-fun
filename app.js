453cde68b5db40de2c07a88cdf283edf8e0f0cb0
Marek <marekficht@gmail.com>
2018-10-06 19:46:59 +0200

    Fixed future gallery. Btns worked, SASS for gallery fixed.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import './main.scss';
 $(document).ready( function () {
     console.log('%c ------ DOM loaded ------', 'color: green; font-weight: bold;');
 
+// ****** NAV BTNS ****** //
     const navBtns = $('.nav li');
     const styles = $('.display-style > div');
     // console.log(navBtns, styles);
@@ -12,10 +13,43 @@ $(document).ready( function () {
         // console.table([this, $(this)]); console.log('click', this, $(this));
 
         styles.addClass('hide-style');
+        divWithFG.addClass('move-gallery');     // Hoisting :)
 
         let chooseStyle = $(this).data('id');
         styles.eq(chooseStyle).removeClass('hide-style');  //0 is for '.waiting'
+    })
 
+// ****** NAV BTNS ****** //
+    const btnFG1 = $('#btn-fg1');
+    const btnFG2 = $('#btn-fg2');
+    const divWithFG = $('.move-fg');
+    let currentFG = 1;
+    // --- solving problem with smooth animation on start/end
+    let smoothTransition = 60;
+
+    btnFG1.click( function () {
+        divWithFG.attr('class', 'move-fg');
+
+        // currentFG--;
+        // currentFG < 1 ? currentFG = 6 : currentFG;
+        // divWithFG.addClass(`show-fg${currentFG}`);
+
+        // --- solving problem with smooth animation on start/end
+        smoothTransition += 60;
+        divWithFG.addClass(`show-fg${currentFG}`).css('transform', `rotateY(${smoothTransition}deg)`);
+
+    })
+
+    btnFG2.click( function () {
+        divWithFG.attr('class', 'move-fg');
+
+        // currentFG++;
+        // currentFG > 6 ? currentFG = 1 : currentFG;
+        // divWithFG.addClass(`show-fg${currentFG}`);
+
+        // --- solving problem with smooth animation on start/end
+        smoothTransition -= 60;
+        divWithFG.addClass(`show-fg${currentFG}`).css('transform', `rotateY(${smoothTransition}deg)`);
     })
 
 })
